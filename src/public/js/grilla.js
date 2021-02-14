ba0617c78dce48a91454c1baa112a6cf4ac26881
Brayan Forero <brayanroswll2802@gmail.com>
2021-02-14 03:37:41 -0400

    2.1 add list player

--- src/public/js/grilla.js
+++ src/public/js/grilla.js
@@ -1,3 +1,24 @@
-const vue = new vue({
+let API = 'https://free-nba.p.rapidapi.com/players'
+let API_KEY = '665ed21c81msh061a123159a7203p1bb178jsn2ddffd2c7093'
+
+const vue = new Vue({
   el: '#app',
+  data: {
+    players: [],
+    metas: {},
+  },
+  methods: {
+    async getAllPlayerApi() {
+      const headers = { 'x-rapidapi-key': API_KEY }
+      let res = await axios.get(API, {
+        headers,
+      })
+
+      this.players = res.data.data
+      this.metas = res.data.meta
+    },
+  },
+  created() {
+    this.getAllPlayerApi()
+  },
 })
