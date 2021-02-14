1863d41c87b634f409a41e35d1f486a71041f88b
Brayan Forero <brayanroswll2802@gmail.com>
2021-02-14 04:59:09 -0400

    2.2 add detail team

--- src/public/js/grilla.js
+++ src/public/js/grilla.js
@@ -1,21 +1,40 @@
-let API = 'https://free-nba.p.rapidapi.com/players'
+let API = 'https://free-nba.p.rapidapi.com/'
 let API_KEY = '665ed21c81msh061a123159a7203p1bb178jsn2ddffd2c7093'
-
+const headers = { 'x-rapidapi-key': API_KEY }
 const vue = new Vue({
   el: '#app',
   data: {
     players: [],
     metas: {},
+    teamShow: [],
+    oldItemSelected: null,
   },
   methods: {
     async getAllPlayerApi() {
-      const headers = { 'x-rapidapi-key': API_KEY }
-      let res = await axios.get(API, {
+      let res = await axios.get(API + 'players', {
+        headers,
+      })
+      let { data } = res.data
+
+      this.players = data.map(e => {
+        e.showDetail = false
+        return e
+      })
+      this.metas = res.data.meta
+    },
+    async getSpecificTeam(index, id) {
+      this.players.map((e, i) => {
+        if (i === index) {
+          return
+        }
+        e.showDetail = false
+      })
+      this.players[index].showDetail = !this.players[index].showDetail
+      let res = await axios.get(API + `teams/${id}`, {
         headers,
       })
 
-      this.players = res.data.data
-      this.metas = res.data.meta
+      this.teamShow = res.data
     },
   },
   created() {
